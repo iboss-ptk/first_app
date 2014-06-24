adfcf2182955a123d507408c297a2c6fc8ae1d41
iboss <supanat.ptk@gmail.com>
2014-06-24 22:38:05 +0700

    Initialize respiratory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ed87f81a51fac4a3276c6996fed6cc9e51df4f530f19e8db40935985cbb71f6ac1f49e43024270ea9c73724fd8602f97e88345c2b4f530e0da883420c01f84ab'
